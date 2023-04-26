5fd0c9ae58008a1ad3d3826deef982e5aab2a5b0
TimM <tmaidans@yahoo.com>
2023-04-26 17:50:23 -0400

    added Instagram class

---
+++ src/officeHours/officeHoursWeek12Tasks/app/Instagram.java
@@ -0,0 +1,16 @@
+package officeHours.officeHoursWeek12Tasks.app;
+
+public class Instagram extends App {
+
+
+    public Instagram(String name, int version) {
+        super(name, version);
+    }
+
+    public void postPicture (){
+
+        System.out.println("Posting picture to Instagram");
+
+    }
+
+}
