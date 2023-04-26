cf135886ac215a2474fa63ed0ecc7b65079154eb
TimM <tmaidans@yahoo.com>
2023-04-26 17:52:26 -0400

    added Youtube class

---
+++ src/officeHours/officeHoursWeek12Tasks/app/Youtube.java
@@ -0,0 +1,15 @@
+package officeHours.officeHoursWeek12Tasks.app;
+
+public class Youtube extends App {
+
+
+    public Youtube(String name, int version) {
+        super(name, version);
+    }
+
+    public void watchVideo (){
+        System.out.println("Watching java tutorial");
+    }
+
+
+}
