9f4ad67941a6e1e4b9c35cdb47ee1cbdab803cb8
TimM <tmaidans@yahoo.com>
2023-05-01 21:09:33 -0400

    updated Teacher class

--- src/dailyClasses/day33_abstraction/employeeTask/Teacher.java
+++ src/dailyClasses/day33_abstraction/employeeTask/Teacher.java
@@ -1,9 +1,13 @@
 package dailyClasses.day33_abstraction.employeeTask;
 
-public class Teacher  {
+public class Teacher extends Employee    {
 
+    public Teacher(String name, int age, char gender, String id, String jobTitle, double salary) {
+        super(name, age, gender, id, jobTitle, salary);
+    }
 
+    @Override
+    public void work() {
 
-
-
+    }
 }
