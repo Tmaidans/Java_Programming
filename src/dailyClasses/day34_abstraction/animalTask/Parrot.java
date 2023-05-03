0953ba764e684cffa0bd33ddf93c58629710d965
TimM <tmaidans@yahoo.com>
2023-05-02 21:15:31 -0400

    created Animal sub classes

---
+++ src/dailyClasses/day34_abstraction/animalTask/Parrot.java
@@ -0,0 +1,15 @@
+package dailyClasses.day34_abstraction.animalTask;
+
+public class Parrot extends Animal {
+
+
+    public Parrot(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Parrot"  + getName() + " is eating parrot food");
+
+    }
+}
