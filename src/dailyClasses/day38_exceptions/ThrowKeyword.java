e84bc4561f020abc5fe4b3d9995e63ed7716586c
TimM <tmaidans@yahoo.com>
2023-05-09 19:52:07 -0400

    Finished Throw keyword class

--- src/dailyClasses/day38_exceptions/ThrowKeyword.java
+++ src/dailyClasses/day38_exceptions/ThrowKeyword.java
@@ -1,15 +1,33 @@
 package dailyClasses.day38_exceptions;
 
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ThrowKeyword {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
         System.out.println("Enter your age: ");
         int age = new Scanner(System.in).nextInt();
 
+        if (age < 0 || age > 150) {
+           throw new InputMismatchException("Invalid age " + age);
+        }
+
+        if (age>=21){
+            System.out.println("Eligible");
+        }else{
+            System.out.println("Ineligible");
+        }
+
+        System.out.println("-----------------------------------------------------------------------");
+
+        throw new RuntimeException("Runtime Exception");
+      //  System.out.println("Stuff"); - unreachable
 
 
     }
+
+
 }
