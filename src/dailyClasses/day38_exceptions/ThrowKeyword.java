cfe089ba902e66aaf0461b569a6875ab81a4593b
TimM <tmaidans@yahoo.com>
2023-05-09 19:39:16 -0400

    Created Throw keyword class

---
+++ src/dailyClasses/day38_exceptions/ThrowKeyword.java
@@ -0,0 +1,15 @@
+package dailyClasses.day38_exceptions;
+
+import java.util.Scanner;
+
+public class ThrowKeyword {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter your age: ");
+        int age = new Scanner(System.in).nextInt();
+
+
+
+    }
+}
--- src/dailyClasses/day38_exceptions/ThrowsKeyword2.java
+++ src/dailyClasses/day38_exceptions/ThrowsKeyword2.java
@@ -1,5 +1,7 @@
 package dailyClasses.day38_exceptions;
 
+import java.io.FileInputStream;
+
 public class ThrowsKeyword2 {
 
     public static void main(String[] args) throws InterruptedException {
@@ -18,6 +20,19 @@ public class ThrowsKeyword2 {
         System.out.println("Hello World");
         pauseFor5Seconds();
         System.out.println("Hello Cydeo");
+    }
+
+    public static void method2() throws  Exception{ //when you have multiple exceptions in one code block
+
+        System.out.println("First Program started");
+
+        Thread.sleep(3000);
+
+        System.out.println("first program ended");
+
+        new FileInputStream( " ");
+
+        Thread.sleep(4000);
     }
 
 
