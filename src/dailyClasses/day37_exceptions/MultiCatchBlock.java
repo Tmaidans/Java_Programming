a1e600be437059355b3d34bacbe128d8499e0e61
TimM <tmaidans@yahoo.com>
2023-05-08 22:59:32 -0400

    Finished day 37 class

--- src/dailyClasses/day37_exceptions/MultiCatchBlock.java
+++ src/dailyClasses/day37_exceptions/MultiCatchBlock.java
@@ -1,9 +1,34 @@
 package dailyClasses.day37_exceptions;
 
+import dailyClasses.day35_polymorphism.transportation.Car;
+
 public class MultiCatchBlock {
 
     public static void main(String[] args) {
 
+        System.out.println("Program 1 started");
+
+        Car car = null;
+
+        try {
+            car.drive();
+        }catch (ArithmeticException e){
+            System.out.println("First catch block");
+            e.printStackTrace();
+        }catch (ClassCastException e){
+            System.out.println("Second catch block");
+            e.printStackTrace();
+        }catch (NullPointerException e) {
+            System.out.println("Third catch block");
+            e.printStackTrace();
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("Forth catch block");
+            e.printStackTrace();
+        }catch (RuntimeException e){
+            System.out.println("Fifth catch block");
+            e.printStackTrace();
+        }
+        System.out.println("Program 1 ended");
 
 
     }
