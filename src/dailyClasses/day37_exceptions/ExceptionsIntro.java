92fed4c6466841fa16dd6af8f3603801f1320fb1
TimM <tmaidans@yahoo.com>
2023-05-08 20:03:56 -0400

    Created day 37 class

---
+++ src/dailyClasses/day37_exceptions/ExceptionsIntro.java
@@ -0,0 +1,13 @@
+package dailyClasses.day37_exceptions;
+
+public class ExceptionsIntro {
+
+    public static void main(String[] args) {
+
+        String str = "Java";
+
+        char ch = str.charAt(250);
+
+    }
+
+}
