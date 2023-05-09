e085d4cec29dbdf44ca75553ec348a46ca87f461
TimM <tmaidans@yahoo.com>
2023-05-09 19:23:38 -0400

    Created Library --> Util --> Sleep method

---
+++ src/dailyClasses/day38_exceptions/Test.java
@@ -0,0 +1,17 @@
+package dailyClasses.day38_exceptions;
+
+import utilities.Library;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Cydeo");
+
+        Library.sleep(5);
+
+        System.out.println("How are you today? ");
+
+    }
+
+}
--- src/dailyClasses/day38_exceptions/ThrowsKeyword2.java
+++ src/dailyClasses/day38_exceptions/ThrowsKeyword2.java
@@ -2,19 +2,19 @@ package dailyClasses.day38_exceptions;
 
 public class ThrowsKeyword2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-
+        method1();
 
     }
 
-    public static void pauseFor5Seconds () throws InterruptedException {
+    public static void pauseFor5Seconds() throws InterruptedException {
 
         Thread.sleep(5000);
 
     }
 
-    public static void method1(){
+    public static void method1() throws InterruptedException {
         System.out.println("Hello World");
         pauseFor5Seconds();
         System.out.println("Hello Cydeo");
