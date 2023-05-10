23a0bd21f9718c9bbf8351058a7639051ca972e9
TimM <tmaidans@yahoo.com>
2023-05-09 20:32:28 -0400

    Created custom exception classes

---
+++ src/dailyClasses/day38_exceptions/CustomExceptions.java
@@ -0,0 +1,27 @@
+package dailyClasses.day38_exceptions;
+
+class CustomCheckedException extends Exception{
+
+}
+
+class CustomUncheckedException extends RuntimeException{
+
+
+}
+
+
+
+
+public class CustomExceptions {
+
+    public static void main(String[] args) {
+
+        try {
+            throw new CustomCheckedException();
+        } catch (CustomCheckedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
