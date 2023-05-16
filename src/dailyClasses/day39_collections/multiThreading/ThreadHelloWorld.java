385be787af5d9aeb7a83530a24228235e77aeb35
Tmaidans <tmaidans@yahoo.com>
2023-05-15 20:51:32 -0400

    Created ThreadHelloWorld class

---
+++ src/dailyClasses/day39_collections/multiThreading/ThreadHelloWorld.java
@@ -0,0 +1,13 @@
+package dailyClasses.day39_collections.multiThreading;
+
+public class ThreadHelloWorld extends Thread {
+
+    @Override
+    public void run() {
+        for (int i = 1; i < 6; i++) {
+            System.out.println("Hello World " + i);
+        }
+    }
+
+
+}
