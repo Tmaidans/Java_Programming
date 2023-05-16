cbf0befcfb6f5dbbe10d29e359c5ce7568eaee76
Tmaidans <tmaidans@yahoo.com>
2023-05-15 21:16:33 -0400

    Created TestMultiThread class

--- src/dailyClasses/day39_collections/multiThreading/ThreadHelloWorld.java
+++ src/dailyClasses/day39_collections/multiThreading/ThreadHelloWorld.java
@@ -6,6 +6,11 @@ public class ThreadHelloWorld extends Thread {
     public void run() {
         for (int i = 1; i < 6; i++) {
             System.out.println("Hello World " + i);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
