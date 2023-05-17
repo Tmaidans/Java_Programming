256113da767d8871076359e883ad78f7637a6b92
Tmaidans <tmaidans@yahoo.com>
2023-05-17 19:27:21 -0400

    Created Queue class

--- src/dailyClasses/day40_collections/CollectionPractice.java
+++ src/dailyClasses/day40_collections/CollectionPractice.java
@@ -14,6 +14,26 @@ public class CollectionPractice {
 
         List<Integer> list4 = new Vector<>();
 
+        System.out.println("--------------------------------------------------------------------");
+
+        Collection<Integer> collection = new ArrayList<>();
+        collection.addAll(Arrays.asList(100, 200, 100, 200, 300, 400, 500, 600));
+
+        System.out.println(collection);
+
+        System.out.println(((ArrayList) collection).get(2));
+
+        //System.out.println((Stack)collection.pop());
+
+        System.out.println("--------------------------------------------------------------------");
+
+        Collection<Integer> collection2 = new HashSet<>();
+        collection2.addAll(Arrays.asList(100, 200, 100, 200, 300, 400, 500, 600));
+        System.out.println(collection2);
+
+        System.out.println(new ArrayList<>(collection2).get(0));
+
+        List<Integer> l = new ArrayList<>(collection2);
 
 
     }
---
+++ src/dailyClasses/day40_collections/QueueDemo.java
@@ -0,0 +1,16 @@
+package dailyClasses.day40_collections;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class QueueDemo {
+
+    public static void main(String[] args) {
+
+        Queue<Integer> queue1 = new PriorityQueue<>();
+
+
+
+    }
+
+}
