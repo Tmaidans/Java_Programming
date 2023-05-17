227c88c56738f5237a13251e9c7682691eb952e6
Tmaidans <tmaidans@yahoo.com>
2023-05-17 18:55:53 -0400

    Created day 40 - Collections practice class

---
+++ src/dailyClasses/day40_collections/CollectionPractice.java
@@ -0,0 +1,21 @@
+package dailyClasses.day40_collections;
+
+import java.util.*;
+
+public class CollectionPractice {
+
+    public static void main(String[] args) {
+
+        List<Integer> list1 = new ArrayList<>();
+
+        List<Integer> list2 = new LinkedList<>();
+
+        List<Integer> list3 = new Stack<>();
+
+        List<Integer> list4 = new Vector<>();
+
+
+
+    }
+
+}
