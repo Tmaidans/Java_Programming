b8f0a44365e36f05b27f7e02631193aec0fad3a5
Tmaidans <tmaidans@yahoo.com>
2023-05-17 21:02:48 -0400

    Explicitly created removeIf method

--- src/dailyClasses/day40_collections/IterableExample.java
+++ src/dailyClasses/day40_collections/IterableExample.java
@@ -1,8 +1,62 @@
 package dailyClasses.day40_collections;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class IterableExample {
 
     public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+
+        list.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 1, 4, 5, 5, 2, 3, 4, 5, 6, 7, 10));
+
+        /*
+        for (Integer each : list) {
+            if (each<5){
+                list.remove(each);
+            }
+        }
+
+
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i)<5){
+                list.remove(i);
+            }
+        }
+
+         */
+
+        System.out.println("-----------------------------------------------------------------");
+
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 1, 4, 5, 5, 2, 3, 4, 5, 6, 7, 10));
+
+        Iterator<Integer> it = list2.iterator();
+
+        while(it.hasNext()){
+           Integer each = it.next();
+           if(each<5){
+               it.remove();
+           }
+        }
+
+        System.out.println(list2);
+
+        System.out.println("----------------------------------------------------------------");
+
+        List<Integer> list3 = new ArrayList<>();
+        list3.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 1, 4, 5, 5, 2, 3, 4, 5, 6, 7, 10));
+
+        list3.removeIf(each -> each<5);
+
+        System.out.println(list3);
+
+
 
 
 
